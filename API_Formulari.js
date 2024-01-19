9c7df71d7942a1d98379a17b4355fda50f6dc569
Aakriti313 <dobleagueber313@gmail.com>
2024-01-19 16:11:31 +0100

    Parte Aakriti :)

--- API_Formulari.js
+++ API_Formulari.js
@@ -1,53 +1,72 @@
-//    AAKRITI LA MEJOR
 //a. Validació amb JS bàsic
-// i. Cóm accedim als elements d’un formulari segons el name?
-    /*Para poder acceder a un elemento de un formulario mediante el nombre,
-    primero debemos crearlo en el HTML, a éste le pondremos el atributo "name".
-    Después, en el archivo .js acceder al formulario utilizando el método "document.getElementByName()"
-    dónde entre los préntesis indicamos el monbre del formulario.
-    Este método obtendrá todos los elementos con el nombre indicado*/
 
+// i. Cóm accedim als elements d’un formulari segons el name?
+
+  /*Hemos de utilitzar el document.forms y acceder al formulario por el atributo name.*/
     //EJ:
-    document.getElementById("button").addEventListener("click", function() {
-    let form = document.forms["myForm"];
-    let name = form["name"].value;
+    document.getElementById("name").addEventListener("change", function() {
+    let name = document.forms["myForm"]["name"].value;
     console.log(name);
-    document.getElementById("namer").innerHTML = "Nombre: " + name;
+    document.getElementById("namer").innerHTML = name ;
     })
 
 // ii. Cóm podem validar un input a mesura que l’usuari vagi escrivint
-    /*Para validad un input a medida que el usuario va escribiendo, hemos de añadir el atributo "novalidate"
-    al formulario.*/
+
+    /*Hemos de añadir el atributo "novalidate" al formulario y con el evento onInput o onChange validamos*/ 
     //EJ:
-    document.getElementById("name").addEventListener("input", function() {
-        nameValidation();
+    let form = document.forms["myForm"];
+
+    form.addEventListener("submit", function (event) {
+      if(!form.checkValidity()){
+        event.preventDefault();
+        alert("No se ha enviado el formulario. Rellene todos los datos")
+      }
+    })
+
+// iii. Cóm podem validar un input al perdre el “focus”
+    
+  /*Utilizamos el evento onBlur o onFocus*/
+    //EJ:
+    let surnames = document.forms["myForm"]["surnames"];
+
+    surnames.addEventListener("blur", function(){
+      let surnames_data = surnames.value;
+
+      if (surnames_data == "") {
+        document.getElementById("surnamesr").innerHTML = "Rellena el campo";
+      } else {
+        document.getElementById("surnamesr").innerHTML = "";
+      }
     });
 
-    function nameValidation() {
-        let form = document.forms["myForm"];
-        let name = form["name"].value;
-        
-        var nomInput = document.getElementById("name");
-        var errorSpan = document.getElementById("nameError");
-      
-        // Ejemplo de validación simple: el nombre no puede ser vacío
-        if (nomInput.value.trim() === "") {
-          errorSpan.textContent = "El nom no pot ser buit";
-        } else {
-          errorSpan.textContent = "";
-        }
-      } 
-// iii. Cóm podem validar un input al perdre el “focus”
-    /*onBlur / onFocus: quan es perd o es guanya el “focus”*/
 // iv. Cóm evitem enviar un formulari si hi ha algun error?
-    /*invalid*/
-// v. Cóm funcionen les expressions regulars amb JS i quina relació tenen amb la
-// validació de formularis?
+
+    /*Con el evento onsubmit o addEventListener evitamos enviar un formulario si tiene errores.*/
+    //EJ (onSubmit):
+    function validation(){
+      if (document.forms["myForm"]["email"].value !="ex@ex.com") {
+        document.getElementById("emailr").innerHTML = "Formato inválido" ;
+        return false;
+      }
+    }
+
+// v. Cóm funcionen les expressions regulars amb JS i quina relació tenen amb la validació de formularis?
+  
+  /* */
+    //EJ:
+    let email = document.forms["myForm"]["email"];
+
+    email.addEventListener("change", function(){
+      let email_data = email.value
+      let pattern = new RegExp("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/");
+      if (!pattern.test(email_data)) {
+        document.getElementById("emailr").innerHTML = "Formato inválido" ;
+      }else{
+        document.getElementById("emailr").innerHTML = " " ;
+      }
+    })
 
 
-
-
-// Valery LA MEJOR ESQUIZO
 // b. API JS Formulari
 // i. A on indiquem les restriccions dels inputs del formulari?
 document.getElementById("myForm").addEventListener("submit", function(event) {
